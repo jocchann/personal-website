452ee2bbe28fc845c08414ae967006da64788976
Joanna Chan <joanna.chan@live.ca>
2019-09-14 20:14:21 -0400

    remove unused icon, update theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,10 +35,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#a566ed`,
+        theme_color: `#a566ed`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
